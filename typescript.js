127de1c5802cd529ea4a14972fd1d92aa74c34f0
experiencei <ayelojahighbee01@gmail.com>
2021-12-07 23:44:49 +0100

    working with TS on small proj.

---
+++ typescript.js
@@ -0,0 +1,5 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var ans = sum(8, 9);
+console.log(ans);
---
+++ typescript.ts
@@ -0,0 +1,7 @@
+const sum = (a : number , b : number ) => {
+    return a + b
+}
+
+const ans = sum(8 , 9)
+
+console.log(ans);
