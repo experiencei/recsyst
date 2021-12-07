2d5a90981e19cd8095096d081c7f92e61a7b61e3
experiencei <ayelojahighbee01@gmail.com>
2021-12-08 00:16:55 +0100

    working with Ts on small proj.

--- typescript.js
+++ typescript.js
@@ -3,3 +3,32 @@ var sum = function (a, b) {
 };
 var ans = sum(8, 9);
 console.log(ans);
+//boolean
+var isCool = true;
+//number
+var age = 34;
+//string
+var eyeColor = "brown";
+var favouriteQuotes = "".concat(age, " year's old");
+//array
+var pets = ["cats", "dog", "Rabbit"];
+var pets2 = ["lions", "Dragon", "Lizards"];
+//object
+var wizards = {
+    a: "nathaniels"
+};
+//null & undefined
+var meh = undefined;
+var noo = null;
+//Tuple
+var basket;
+basket = ["basketball", 5];
+//Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["large"] = 3] = "large";
+})(Size || (Size = {}));
+;
+var sizeName = Size.Medium;
--- typescript.ts
+++ typescript.ts
@@ -5,3 +5,34 @@ const sum = (a : number , b : number ) => {
 const ans = sum(8 , 9)
 
 console.log(ans);
+
+//boolean
+let isCool: boolean = true;
+
+//number
+let age: number = 34;
+
+//string
+let eyeColor: string = "brown";
+let favouriteQuotes: string = `${age} year's old`;
+
+//array
+let pets: string[] = ["cats" , "dog" , "Rabbit"];
+let pets2: Array<string> = ["lions" , "Dragon" , "Lizards"];
+
+//object
+let wizards: object = {
+    a : "nathaniels"
+}
+
+//null & undefined
+let meh: undefined = undefined;
+let noo: null = null;
+
+//Tuple
+let basket: [string , number];
+basket  = ["basketball" , 5];
+
+//Enum
+enum Size {Small = 1, Medium = 2, large = 3};
+let sizeName: number = Size.Medium
